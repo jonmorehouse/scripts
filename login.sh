cd0ec1a483615c77b4461cc60e48ac39d95fb3bc
Jon Morehouse <morehousej09@gmail.com>
2014-01-19 16:09:34 -0800

    Update login script

--- login.sh
+++ login.sh
@@ -1,7 +1,18 @@
 #!/usr/local/bin/zsh
 
+# bootstrap our environment
+#\. source $HOME/.zshrc
+
 # This script is called at startup by a launch agent - symlinks/startup.plist
 $HOME/dotfiles/scripts/dock.scpt
 
 # now lets start up various tmux sessions
-mux start personal
+#mux start personal
+# only start up the personal set up if we don't have a server in place 
+if [ ! `tmux list-session` ] 
+then
+	
+	mux personal
+
+fi
+	
