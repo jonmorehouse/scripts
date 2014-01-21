0e57f4d8f9f4d7b2fbf97cde3306231c6d58b734
Jon Morehouse <morehousej09@gmail.com>
2014-01-20 16:01:02 -0800

    Update brew backup script

--- brew_backup.sh
+++ brew_backup.sh
@@ -1,26 +1,44 @@
 #!/usr/local/bin/zsh
 
-brewfile=$HOME/dotfiles/Brewfile
+# initialize brewfile output
+if [[ -n $1 ]]
+then
+	brewfile=$1
+else
+	
+	brewfile="Brewfile"
+fi
 
-installed=()
-
-# fill our list of brewfiled - dependencies
-grep install $brewfile | while read -r line 
+# now lets create all of the taps needed
+for tap in `brew tap`
 do
-	formula=`echo $line | awk '{ split($0,a, " "); print a[2] }'`
+	echo "tap $tap" >> $brewfile	
 
-	installed+=($formula)
-done 
+done
 
-# grab all dependencies
-for dep in `brew list`
+# we need to update after we tap to ensure all formulae can be found
+echo "update" >> $brewfile
+
+# download all the formulae and parse out their options
+for formula in `brew list`
 do
-	formula=`echo $dep | awk '{ split($0,a," "); print a[1] }'`
+	# initialize options
+	build_info=$(brew info $formula | grep "Built from source with: --")
 
-	if [[ -n `brew info $formula | grep "Built from source with: --"` ]]
+	# print out basic install -- ie: no options
+	if [[ ! -n $build_info ]]
 	then
-		options=`brew info $formula | grep "Built from source with: --"` #| sed 's/"Built from source with"//g'`
-		echo $options
+		echo "install $formula" >> $brewfile
+		continue
 	fi
+
+	# now lets grab the options that were declared there!		
+	# format the options properly
+	build_options=$(echo $build_info | sed 's/Built from source with://' | sed 's/^[ \t]*//')
+
+	# now lets create our entry 
+	formula_entry="install $formula $build_options"
+
+	echo $formula_entry >> $brewfile	
 done
 
