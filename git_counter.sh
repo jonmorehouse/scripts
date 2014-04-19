3bf6c878732c867f3ea1ab919d0308f178964f08
Jon Morehouse <morehousej09@gmail.com>
2014-04-19 15:11:29 -0700

    Update counter

--- git_counter.sh
+++ git_counter.sh
@@ -2,17 +2,17 @@
 
 # this script should be called whenever a git commit happens
 # useful for tracking my statistics for all projects
-
 origin=$(git config --get remote.origin.url)
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-# now 
+# generate filepath
 file=$(date | awk '{ print tolower($1)"-"tolower($2)"-"$3 }')
 file_path=$HOME/.commits/$file
+
+# generate message
 message="$origin#$branch $(date)"
 
-echo $message
-echo $file_path
-
+# update the correct file
+echo $message >> $file_path
 
 
