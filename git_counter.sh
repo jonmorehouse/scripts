75fbc8388547d6783f7dc63b736f493117d9b4fb
Jon Morehouse <morehousej09@gmail.com>
2014-04-19 14:57:25 -0700

    Update git counter

--- git_counter.sh
+++ git_counter.sh
@@ -3,7 +3,17 @@
 # this script should be called whenever a git commit happens
 # useful for tracking my statistics for all projects
 
-BASE_DIR=$(git rev-parse --show-toplevel)
-echo $BASE_DIR
+origin=$(git config --get remote.origin.url)
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+# now 
+filename=$HOME/.commits/$(date | awk '{ print $1" "$2" "$3 }')
+message="$origin \@ $branch \@ $(date)"
+
+echo $origin
+echo $filename
+echo $branch
+echo $message
+
 
 
