5385d4347f185b27aaf5de5cfdf788cc45649c03
Jon Morehouse <morehousej09@gmail.com>
2014-04-19 14:40:40 -0700

    Temp

--- git_counter.sh
+++ git_counter.sh
@@ -3,6 +3,7 @@
 # this script should be called whenever a git commit happens
 # useful for tracking my statistics for all projects
 
-echo $GIT_DIR
+BASE_DIR=$(git rev-parse --show-toplevel)
+echo $BASE_DIR
 
 
